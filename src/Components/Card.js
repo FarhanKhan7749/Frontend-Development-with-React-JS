2b3e7865d692b035d9adc43a247beada3ae85655
FarhanKhan7749 <fk828633@gmail.com>
2023-01-15 17:15:39 +0530

    Children Props and Behind the Scenes

---
+++ src/Components/Card.js
@@ -0,0 +1,8 @@
+import './Card.css'
+function Card(props){
+    //Must add white space otherwise it will not work or you can say not style is not apply to components. 
+    const classes = 'card ' + props.className;
+    return <div className={classes}>{props.children}</div>
+}
+
+export default Card;
